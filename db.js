5163b19f721663f524e4c8a220d2d8b219a0fc21
lucasdu330033-pixel <lucasdu330033@gmail.com>
2025-10-28 06:12:58 +0100

    Create db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connexion à MongoDB réussie");
+  } catch (error) {
+    console.error("❌ Erreur de connexion à MongoDB :", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
